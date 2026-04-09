00081ba40aa2691f159e3c4c84a287b2591dae29
PhiTux <27566312+PhiTux@users.noreply.github.com>
2026-04-09 18:27:53 +0200

    new pin working

---
+++ frontend/src/lib/map/NewPinPopup.svelte
@@ -0,0 +1,39 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let { initialValue = '', onSave = () => {}, onChange = () => {} } = $props();
+
+	onMount(() => {
+		document.querySelector('#newPinTextInput')?.focus();
+	});
+
+	function handleInput(event) {
+		initialValue = event.target.value;
+		onChange(initialValue);
+	}
+
+	function handleSave(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		onSave(initialValue);
+	}
+
+	function handleKeydown(event) {
+		if (event.key === 'Enter') {
+			handleSave(event);
+		}
+	}
+</script>
+
+<div class="new-pin-popup">
+	<input
+		id="newPinTextInput"
+		type="text"
+		class="form-control"
+		placeholder="Name"
+		{initialValue}
+		oninput={handleInput}
+		onkeydown={handleKeydown}
+	/>
+	<button type="button" class="btn btn-success" onclick={handleSave}>Speichern</button>
+</div>
